023429e7d584e7f4632cf58668ee92ff76607359
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-01-28 17:19:47 -0500

    built out the whole application but need to adjust the generateREADME function to the correct README format

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
-import inquirer from "inquier";
-import fs from "fs";
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const generateREADME = data =>
+  `
+${data.username}
+${data.email}
+${data.project}
+${data.description}
+${data.license}
+${data.dependencies}
+${data.tests}
+${data.information}
+${data.contribution}
+`
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+    },
+    {
+      type: "input",
+      name: "project",
+      message: "What is your project's name?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project:"
+    },
+    {
+      type: "list",
+      name: "lisence",
+      message: "What kind of license should your project have?",
+      choices: [
+        { value: "MIT" },
+        { value: "Apache 2.0" },
+        { value: "GPL 3.0" },
+        { value: "BSD 3" },
+        { value: "None" }
+      ]
+    },
+    {
+      type: "input",
+      name: "dependencies",
+      message: "What command should be run to install dependencies?",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command should be run to run tests?",
+      default: "npm test"
+    },
+    {
+      type: "input",
+      name: "information",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What does the user need to know about contributing to the repo?",
+    }
+  ])
+  .then((data) => {
+    fs.writeFile(`${data.username}-README.md`, generateREADME(data), (err) => {
+      err ? console.error(err) : console.log("Generating README...");
+    });
+  });
